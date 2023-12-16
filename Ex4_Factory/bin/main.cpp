8191bcf607409d814b72677a9041ce5919db38fd
P4r4dis <adil.d.pro@gmail.com>
2023-12-16 14:35:01 +0100

    [ADD] - implementation main function

--- Ex4_Factory/bin/main.cpp
+++ Ex4_Factory/bin/main.cpp
@@ -1,8 +1,31 @@
 #include "../include/Droid.hpp"
 #include "../include/DroidMemory.hpp"
 #include "../include/Carrier.hpp"
+#include "../include/Supply.hpp"
 
 int main(void)
 {
+    Droid **w = new Droid*[10];
+    char c = '0';
+
+    for (int i = 0; i < 3; ++i)
+        w[i] = new Droid(std::string("wreck: ") + (char)(c + i));
+
+    Supply s1(Supply::Silicon, 42);
+    Supply s2(Supply::Iron, 70);
+    Supply s3(Supply::Wreck, 3, w);
+
+    std::cout << s3 << std::endl;
+
+    size_t s = s2;
+    std::cout << s << std::endl;
+    std::cout << *(*(--s3)) << std::endl;
+    std::cout << *(++s3)->getStatus() << std::endl;
+    ++s3;
+    *s3 = 0;
+    std::cout << *s3 << std::endl;
+    std::cout << s2 << std::endl;
+    std::cout << !s3 << std::endl;
+    
     return 0;
 }
